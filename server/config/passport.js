fab6a7368e4a70c9adc01a1d6223fc9c8815501c
Iheuzio <97270760+Iheuzio@users.noreply.github.com>
2024-02-25 19:23:30 -0500

    Refactor file structure

---
+++ server/config/passport.js
@@ -0,0 +1,41 @@
+const session = require('express-session');
+const SteamStrategy = require('../lib/passport-steam').Strategy;
+
+module.exports = function(passport, app) {
+  // Passport session setup.
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+
+  passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+  });
+
+  // Use the SteamStrategy within Passport.
+  passport.use(new SteamStrategy({
+    returnURL: 'http://localhost:3000/auth/steam/return',
+    realm: 'http://localhost:3000/',
+    apiKey: '1EDC0D204A7716E809F0B2DABE207BE7'
+  },
+  function(identifier, profile, done) {
+    process.nextTick(function () {
+      profile.identifier = identifier;
+      return done(null, profile);
+    });
+  }
+  ));
+
+  // configure Express
+  app.set('views', __dirname + '/../views');
+  app.set('view engine', 'ejs');
+
+  app.use(session({
+    secret: 'your secret',
+    name: 'name of session id',
+    resave: true,
+    saveUninitialized: true
+  }));
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+};
