1c8d0ec9cacfa751efe976da45dbb4a37f93d7fc
Christopher Bartos <christopher.bartos@dawsoncollege.qc.ca>
2024-02-27 14:24:34 -0500

    User must be logged in to view main content on site

--- server/routes/auth-steam.js
+++ server/routes/auth-steam.js
@@ -3,13 +3,13 @@ const passport = require('passport');
 const router = express.Router();
 
 router.get('/auth/steam',
-  passport.authenticate('steam', { failureRedirect: '/' }),
+  passport.authenticate('steam', { failureRedirect: '/account' }),
   function(req, res) {
     res.redirect('/');
   });
 
 router.get('/auth/steam/return',
-  passport.authenticate('steam', { failureRedirect: '/' }),
+  passport.authenticate('steam', { failureRedirect: '/account' }),
   function(req, res) {
     res.redirect('/');
   });
