0f2e5c37094e110f37db49f1d280c9dc3eb70ab7
Shlomo Bensimhon <7024205-shlomy_b@users.noreply.gitlab.com>
2024-02-27 15:30:48 -0500

    work on fetching games to main page

--- client/src/components/HorizontalScroller.js
+++ client/src/components/HorizontalScroller.js
@@ -1,14 +1,40 @@
 import './HorizontalScroller.css';
-
+import { useState, useEffect } from 'react'
 
 
 export default function Scroller() {
   
 
+  const [games, setGames] = useState([])
 
+  useEffect(() => {
+      async function fetchGames() {
+          try {
+              const response = await fetch('/apiv2/steamgames');
+              if (response.ok) {
+                  const games = await response.json();
+                  setGames(games)
+              } else {
+                  console.log("Error")
+              }
+          } catch (error) {
+              console.log('There was an error', error);
+          }
+      }
+      fetchGames()
+  }, [])
+
+  const gameList = games.map(game =>
+      <li key={game.title}>Game: {game.title}</li>
+  );
+
+ 
 
 
   return <div className="Scroller" >
     <h2>Scroller</h2>    
+    <ul>
+          {gameList}
+      </ul>
   </div>
 }
