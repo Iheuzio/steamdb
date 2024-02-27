6f2f7963e38995f53773a6b8f8397295fa07e35e
Christopher Bartos <christopher.bartos@dawsoncollege.qc.ca>
2024-02-27 18:17:22 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- server/utils/delete.js
+++ server/utils/delete.js
@@ -4,7 +4,7 @@ const DB = require('../db/db');
   let db;
   try {
     db = new DB();
-    await db.connect('test', 'dataset');
+    await db.connect('dataset', 'dataset');
     
     // Pass an empty filter to delete all documents in the collection
     const num = await db.deleteMany({});
