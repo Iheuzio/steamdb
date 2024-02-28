5719824686072f9a896047d3efe40cd7319b65f6
Shlomo Bensimhon <7024205-shlomy_b@users.noreply.gitlab.com>
2024-02-28 11:08:28 -0500

    Added test structure for route unit tests along with a test

---
+++ server/__tests__/route_tests/user.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+const app = require('../../app.js');
+const DB = require('../../db/db.js');
+
+jest.mock('../../db/db');
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+//Unit test for calling the mock api
+
+describe('GET /steamgames/:2050650', () => {
+  test('It should respond the game api id', async () => {
+    jest.spyOn(DB.prototype, 'readBySteamAPIId').mockResolvedValue(
+      {'title' : 'Resident Evil 4'}
+    );
+
+    const response = await request(app).get('/apiv2/steamgames/2050650');
+    expect(response.body).toEqual(
+      {'title' : 'Resident Evil 4'}
+    );
+    expect(response.statusCode).toBe(200);
+    expect(response.type).toEqual('application/json');
+  });
+});
