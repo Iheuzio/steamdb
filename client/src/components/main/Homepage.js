d8a07874dae12f5bb85326c1961b2943842eaeeb
LucaFratipietro <luca.fratipietro@dawsoncollege.qc.ca>
2024-02-28 09:46:36 -0500

    Added React-Router to the index.js page

--- client/src/components/main/Homepage.js
+++ client/src/components/main/Homepage.js
@@ -1,20 +1,11 @@
 import './Homepage.css';
-import Scroller from './HorizontalScroller';  
-import Graph from '../game_details/GameGraph';
-import GameReview from '../game_details/GameReview';
+import Scroller from './HorizontalScroller'; 
 
 
 
 export default function Homepage() {
   
-
-
-
-
   return <div className="Home" >
     <Scroller />
-    
-    <Graph />
-    <GameReview />
   </div>
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
-import App from './App';
+import Homepage from './components/main/Homepage';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
