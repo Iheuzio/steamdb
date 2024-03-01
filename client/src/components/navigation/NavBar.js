3185f63a706d5c9bcae88f0c94158b2dbbbce479
lucafratipietro <luca.fratipietro@dawsoncollege.qc.ca>
2024-02-29 21:22:37 -0500

    Added image and some styling to the Navbar

--- client/src/components/navigation/NavBar.js
+++ client/src/components/navigation/NavBar.js
@@ -1,17 +1,26 @@
 import './NavBar.css';
 import { Link } from "react-router-dom";
+import logo from '../../static-images/steamdb.png'
+
 
 export default function NavBar() {
   return (
     <>
-      <div id='navbar'/>
         <nav>
-          <ul>
+          <ul id='nav-ul'>
             <li>
-              <Link to={`/`}> Home </Link>
+              <Link to={`/`}>
+                <img src={logo} alt='website logo'/>
+              </Link>
             </li>
             <li>
               <Link to={`/search`}> Search </Link>
+            </li>
+            <li>
+              <Link to={`/TODO`}> Lists </Link>
+            </li>
+            <li>
+              <Link to={`/TODO`}> Profile </Link>
             </li>
           </ul>
         </nav>
--- client/src/components/search/SearchPage.js
+++ client/src/components/search/SearchPage.js
@@ -40,14 +40,18 @@ export default function SearchPage() {
         setResults(filteredGames);
     }
     
-    return <div className="SearchPage">
-        <NavBar/>
-        <Search results={results}
+    return (
+    <>
+        <NavBar />
+        <div className="SearchPage">
+            <Search results={results}
                 setResults={setResults}
                 filters={filters}
                 setFilters={setFilters}
                 filterFields={filterFields}
                 updateFilters={updateFilters} />    
-        <GenreFilters updateFilters={updateFilters} />
-    </div>
+            <GenreFilters updateFilters={updateFilters} />
+        </div>
+    </>
+    )
 }
