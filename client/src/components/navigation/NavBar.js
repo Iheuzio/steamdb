b5a2d4e6781821e852e12975bc68f2ec33679854
lucafratipietro <luca.fratipietro@dawsoncollege.qc.ca>
2024-02-29 22:06:04 -0500

    Added rest of css for the navbar

--- client/src/components/navigation/NavBar.js
+++ client/src/components/navigation/NavBar.js
@@ -9,19 +9,21 @@ export default function NavBar() {
         <nav>
           <ul id='nav-ul'>
             <li>
-              <Link to={`/`}>
+              <Link className='logo' to={`/`}>
                 <img src={logo} alt='website logo'/>
               </Link>
             </li>
-            <li>
-              <Link to={`/search`}> Search </Link>
-            </li>
-            <li>
-              <Link to={`/TODO`}> Lists </Link>
-            </li>
-            <li>
-              <Link to={`/TODO`}> Profile </Link>
-            </li>
+            <div id = 'nav-nonlogo'>
+              <li>
+                <Link className='link' to={`/search`}> Search </Link>
+              </li>
+              <li>
+                <Link className='link' to={`/TODO`}> Lists </Link>
+              </li>
+              <li>
+                <Link className='link' to={`/TODO`}> Profile </Link>
+              </li>
+            </div>
           </ul>
         </nav>
     </>
