76d83770afb6259da1b2dcd2a9ee5963f9ee2fa2
Shlomo Bensimhon <sbshlomo@hotmail.com>
2024-03-03 16:26:21 -0500

    debugging hanging test

--- server/__tests__/route_tests/user.test.js
+++ server/__tests__/route_tests/user.test.js
@@ -8,33 +8,27 @@ beforeEach(() => {
   jest.clearAllMocks();
 });
 
-// Unit test for calling the mock API
-describe('GET /localapi/:2050650', () => {
-  test('It should respond with the game API id', async () => {
-    jest.spyOn(DB.prototype, 'readBySteamAPIId').mockResolvedValue({
-      'title': 'Resident Evil 4'
-    });
+//Unit test for calling the mock api
 
-    try {
-      // Making the request to the mocked API endpoint
-      const response = await request(app).get('/localapi/steamgames/2050650');
+describe('GET /steamgames/:2050650', () => {
+  test('It should respond the game api id', async () => {
+    jest.spyOn(DB.prototype, 'readBySteamAPIId').mockResolvedValue(
+      {'title' : 'Resident Evil 4'}
+    );
 
+    const response = await request(app).get('/localapi/steamgames/2050650');
+    expect(response.body).toEqual(
+      {'title' : 'Resident Evil 4'}
+    );
+    expect(response.statusCode).toBe(200);
+    expect(response.type).toEqual('application/json');
 
-      expect(response.body).toEqual({
-        'title': 'Resident Evil 4'
-      });
-      expect(response.statusCode).toBe(200);
-      expect(response.type).toEqual('application/json');
-
-    } catch (error) {
-      console.error('Test error:', error);
-      throw error; 
-    }
-  });
-
-  
-  // Disconnect from the database after all tests are done
-  afterAll(async () => {
     await DB.prototype.close();
   });
 });
+
+
+afterEach(async () => {
+  await DB.prototype.close();
+});
+
