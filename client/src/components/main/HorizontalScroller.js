997579a1f0eee9a325374e45fcaf53e5d1112e2e
Marko <markolitovchenko24@gmail.com>
2024-03-03 21:48:21 -0500

    Added images to horizontal slider

--- client/src/components/main/HorizontalScroller.js
+++ client/src/components/main/HorizontalScroller.js
@@ -1,34 +1,28 @@
 import './HorizontalScroller.css';
 
-
-
 export default function HorizontalScroller() {
+    
     return <div>
-        <h1 class="mt-12 mb-4 font-semibold  text-xl">Horizontal Silder</h1>
+        <h1 class="mt-12 mb-4 font-semibold  text-xl">Horizontal Game Slider</h1>
         <div class="slider">
         <div class="slide-track2">
-            <img class="slide bg-red-500" src="https://clan.akamai.steamstatic.com/images/41316928/f7f5587c77587cc8ae5eb50e39defcbcc3acaa88.jpg"/>
-            <img class="slide bg-purple-500"/>
-            <img class="slide bg-blue-500"/>
-            <img class="slide bg-green-500"/>
-            <img class="slide bg-pink-500"/>
-            <img class="slide bg-red-500"/>
-            <img class="slide bg-purple-500"/>
-            <img class="slide bg-blue-500"/>
-            <img class="slide bg-green-500"/>
-            <img class="slide bg-pink-500"/>
-            <img class="slide bg-red-500"/>
-            <img class="slide bg-purple-500"/>
-            <img class="slide bg-blue-500"/>
-            <img class="slide bg-green-500"/>
-            <img class="slide bg-pink-500"/>
-            
+            { Array.from({ length: 20 }, (_, index) => <ImageSet key={index} />) }
         </div>
         </div>
     </div>
 }
 
-// export default function Scroller() {
+function ImageSet() {
+    return <>
+        <img class="slide bg-red-500" src="https://cdn.cloudflare.steamstatic.com/steam/apps/553850/header.jpg?t=1707955699"/>
+        <img class="slide bg-purple-500" src="https://cdn.cloudflare.steamstatic.com/steam/apps/730/header.jpg?t=1698860631"/>
+        <img class="slide bg-blue-500" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1086940/header.jpg?t=1705604554"/>
+        <img class="slide bg-green-500" src="https://cdn.cloudflare.steamstatic.com/steam/apps/899770/header.jpg?t=1709060046"/>
+        <img class="slide bg-pink-500" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1245620/capsule_sm_120.jpg?t=1709086268"/>
+    </>
+}
+
+// function TempScroller() {
   
 
 //   const [games, setGames] = useState([])
@@ -36,7 +30,7 @@ export default function HorizontalScroller() {
 //   useEffect(() => {
 //       async function fetchGames() {
 //           try {
-//               const response = await fetch('/apiv2/steamgames');
+//               const response = await fetch('/localapi/steamgames');
 //               if (response.ok) {
 //                   const games = await response.json();
 //                   setGames(games)
@@ -49,11 +43,11 @@ export default function HorizontalScroller() {
 //       }
 //       fetchGames()
 //   }, [])
-
+//   console.log(games);
 //   const gameList = games.map(game =>
 //       <li key={game.title}>Game: {game.title}</li>
 //   );
-
+// }
  
 
 
