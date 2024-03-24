27c84838bc656cd466bd36c54cbeea1408dc7f13
Marko <markolitovchenko24@gmail.com>
2024-03-23 21:03:50 -0400

    Implemented Christopher's styling for sidebar toggling

--- client/src/components/search/SearchPage.js
+++ client/src/components/search/SearchPage.js
@@ -40,17 +40,29 @@ export default function SearchPage() {
         setResults(filteredGames);
     }
     
+    const [sidebarOpen, setSidebarOpen] = useState(true);
+
+    const toggleSidebar = () => {
+        setSidebarOpen(!sidebarOpen);
+    };
+
     return (
     <>
         <NavBar />
         <div className="SearchPage">
+            <button className="toggle-sidebar" onClick={toggleSidebar}>
+                {sidebarOpen ? '<' : '>'}
+            </button>
+            <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
+                <GenreFilters updateFilters={updateFilters} />
+            </div>
             <Search results={results}
                 setResults={setResults}
                 filters={filters}
                 setFilters={setFilters}
                 filterFields={filterFields}
                 updateFilters={updateFilters} />    
-            <GenreFilters updateFilters={updateFilters} />
+            {/* <GenreFilters updateFilters={updateFilters} /> */}
         </div>
     </>
     )
