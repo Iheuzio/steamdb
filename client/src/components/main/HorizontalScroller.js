46c040c5a8c04abce612acbcc413de760a22e62b
Shlomo Bensimhon <7024205-shlomy_b@users.noreply.gitlab.com>
2024-04-02 14:08:47 -0400

    Fixed minor details in main scroller

--- client/src/components/main/HorizontalScroller.js
+++ client/src/components/main/HorizontalScroller.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import './HorizontalScroller.css';
 import {Link} from 'react-router-dom';   // Import the Link component
 
@@ -6,22 +7,11 @@ export default function HorizontalScroller({results}) {
         <h1 class="mt-12 mb-4 font-semibold  text-xl">Horizontal Game Slider</h1>
         <div class="slider">
         <div class="slide-track2">
-            {/* { Array.from({ length: 50 }, (_, index) => <ImageSet key={index} />) } */}
             { results.map(result => <SearchResult key={result.id} result={result} />) } 
 
         </div>
         </div>
     </div>
-}
-
-function ImageSet() {
-    return <>
-        <img class="slide bg-red-500" src="https://cdn.cloudflare.steamstatic.com/steam/apps/553850/header.jpg?t=1707955699"/>
-        <img class="slide bg-purple-500" src="https://cdn.cloudflare.steamstatic.com/steam/apps/730/header.jpg?t=1698860631"/>
-        <img class="slide bg-blue-500" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1086940/header.jpg?t=1705604554"/>
-        <img class="slide bg-green-500" src="https://cdn.cloudflare.steamstatic.com/steam/apps/899770/header.jpg?t=1709060046"/>
-        <img class="slide bg-pink-500" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1245620/capsule_sm_120.jpg?t=1709086268"/>
-    </>
 }
 
 function SearchResult({ result }) {
@@ -30,7 +20,7 @@ function SearchResult({ result }) {
 
     return <div>
         <Link to={`/details?game=${api}`}>
-        <a> <img src={ result.image_url}  class="slide bg-pink-500" alt='image' width="50px" /></a>
+        <img src={ result.image_url}  class="slide bg-pink-500" alt='image' width="50px" />
         </Link>
     </div>
 }
