2fc2298caa8ddef18983ca967b71db2dcebba8c8
Christopher Bartos <christopher.bartos@dawsoncollege.qc.ca>
2024-04-04 10:48:26 -0400

    Remove unused profile page code

--- client/src/components/profile/ProfilePage.js
+++ client/src/components/profile/ProfilePage.js
@@ -1,42 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import NavBar from '../navigation/NavBar';
 
-async function getUserLogin() {
-  // fetch details from /account
-  try {
-    const response = await fetch('/account', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    if (response.status === 401) {
-      throw new Error('Unauthorized');
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    // Launch the /auth/steam window
-    const authWindow = window.open('/auth/steam', '_blank');
-
-    // Wait for the /auth/steam window to close
-    while (!authWindow.closed) {
-      await new Promise(resolve => setTimeout(resolve, 1000));
-    }
-
-    // Get user login from /account
-    const response = await fetch('/account', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const data = await response.json();
-    console.log('Success:', data);
-    return data;
-  }
-}
-
 export default function ProfilePage() {
   const [account, setAccount] = useState(null);
 
