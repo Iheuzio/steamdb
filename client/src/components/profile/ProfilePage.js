8599b311f9f0691cf2d74838110799172e010a44
Christopher Bartos <christopher.bartos@dawsoncollege.qc.ca>
2024-04-04 10:47:01 -0400

    Add logout and fix redirect to steam page

--- client/src/components/profile/ProfilePage.js
+++ client/src/components/profile/ProfilePage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import NavBar from '../navigation/NavBar';
 
 async function getUserLogin() {
   // fetch details from /account
@@ -40,19 +41,35 @@ export default function ProfilePage() {
   const [account, setAccount] = useState(null);
 
   useEffect(() => {
-    getUserLogin().then(data => setAccount(data)).catch(error => console.error('Error:', error));
+    fetch('/account')
+        .then(response => {
+            if (response.status === 401) {
+                window.location.href = '/auth/steam';
+            } else {
+                return response.json();
+            }
+        })
+        .then(data => {
+            if (data) {
+                setAccount(data);
+            }
+        })
+        .catch(error => console.error('Error:', error));
   }, []);
-
-  if (!account) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <>
-      <h1>Profile Page</h1>
-      <img src={account.user.photos[2].value} alt='Your Avatar Image' />
-      <p>ID: {account.user.id}</p>
-      <p>Name: {account.user.displayName}</p>
+      <NavBar/>
+      {
+        account ? <>
+                    <h1>Profile Page</h1>
+                    <img src={account.user.photos[2].value} alt='Your Avatar Image' />
+                    <p>ID: {account.user.id}</p>
+                    <p>Name: {account.user.displayName}</p>
+                    <button onClick={() => window.location.href = '/logout'}>Logout</button>
+                  </>:
+                    <h1>Loading...</h1> 
+      }
     </>
   );
 }
