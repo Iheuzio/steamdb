6096a4710f838c319b5d0dea2081708ef6f4a37c
Marko <markolitovchenko24@gmail.com>
2024-04-04 11:18:03 -0400

    Refactored steamgames route

--- server/routes/steamIMDBRouter.js
+++ server/routes/steamIMDBRouter.js
@@ -3,14 +3,149 @@ const router = express.Router();
 const DB = require('../db/db');
 
 const db = new DB();
-  
+
+const pageSize = 20; 
+
+router.get('/search/:type', async (req, res, next) => {
+  const page = req.query.page || 1;
+
+  if (isNaN(page) || page < 0 || page > 10000) {
+    res.status(400).json({ error: 'Invalid page number. Page must be between 0 and 10000' });
+    return;
+  }
+
+  next();
+});
+
+router.get('/search/string', async (req, res) => {
+  try {
+    const page = req.query.page;
+    const query = req.query.query || ' ';
+    const field = req.query.field;
+
+    const allowedFields = ['title', 'publisher', 'developer'];
+
+    if (!allowedFields.includes(field)) {
+      res.status(400).json(
+        { error: 'Invalid field parameter. Field must be one of: title, publisher, developer' }
+      );
+      return;
+    }
+
+    const steamGames = await db.readByQuery(field, String(query), pageSize, page);
+
+    if (!steamGames.length) {
+      res.status(404).json({ error: 'No games were found' });
+      return;
+    }
+
+    res.status(200).json(steamGames);
+  } catch (error) {
+    res.status(500).json({ error: 'Something went wrong, try again later' });
+  }
+});
+
+router.get('/search/number', async (req, res) => {
+  try {
+    const page = req.query.page;
+    const query = req.query.query || 5000;
+    const field = req.query.field || 'peak';
+    const operator = req.query.operator || 'lt';
+
+    const allowedFields = ['peak'];
+
+    if (!allowedFields.includes(field)) {
+      res.status(400).json(
+        { error: 'Invalid field parameter. Field must be one of: peak' }
+      );
+      return;
+    }
+
+    if (isNaN(query) || query < 0) {
+      res.status(400).json({ error: 'Invalid query parameter. Query must be a positive number' });
+      return;
+    }
+
+    const allowedOperators = ['lt', 'gt', 'eq'];
+
+    if (!allowedOperators.includes(operator)) {
+      res.status(400).json(
+        { error: 'Invalid operator parameter. Operator must be one of: lt, gt, eq' }
+      );
+      return;
+    }
+
+    const steamGames = await db.readByDateOrNumber(field, query, operator, pageSize, page);
+
+    if (!steamGames.length) {
+      res.status(404).json({ error: 'No games were found' });
+      return;
+    }
+
+    res.status(200).json(steamGames);
+  } catch (error) {
+    res.status(500).json({ error: 'Something went wrong, try again later' });
+  }
+});
+
+router.get('/search/date', async (req, res) => {
+  try {
+    const page = req.query.page;
+    let query = req.query.query || '06-06-2022';
+    const field = req.query.field || 'release_date';
+    const operator = req.query.operator || 'lt';
+
+    const allowedFields = ['release_date'];
+
+    if (!allowedFields.includes(field)) {
+      res.status(400).json(
+        { error: 'Invalid field parameter. Field must be one of: release_date' }
+      );
+      return;
+    }
+
+    query = new Date(query);
+
+    if (!(query instanceof Date && !isNaN(query))) {
+      res.status(400).json(
+        { error: 'Invalid query parameter. Query must be a valid date' +
+        'in the following format: dd-mm-yyyy' }
+      );
+      return;
+    }
+
+    const allowedOperators = ['lt', 'gt', 'eq'];
+
+    if (!allowedOperators.includes(operator)) {
+      res.status(400).json(
+        { error: 'Invalid operator parameter. Operator must be one of: lt, gt, eq' }
+      );
+      return;
+    }
+
+    const steamGames = await db.readByDateOrNumber(field, query, operator, pageSize, page);
+
+    if (!steamGames.length) {
+      res.status(404).json({ error: 'No games were found' });
+      return;
+    }
+
+    res.status(200).json(steamGames);
+  } catch (error) {
+    //console.log(error);
+    res.status(500).json({ error: 'Something went wrong, try again later' });
+  }
+});
+
+
+// currently being refactored / deprecated
 router.get('/steamgames', async (req, res) => {
   try {
     const genre =    req.query.genre;
     const operator = req.query.operator;
     let query =      req.query.query;
     let field =      req.query.field;
-    let page =       req.query.page;
+    const page =       req.query.page;
     let textQuery =  false;
 
     const textFields = ['title', 'publisher', 'developer'];
@@ -20,10 +155,6 @@ router.get('/steamgames', async (req, res) => {
     }
 
     const recordsToSend = 20;
-
-    if (!page || isNaN(page) || page < 0) {
-      page = 1;
-    }
 
     let steamGames = [];
 
